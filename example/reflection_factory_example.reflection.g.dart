ecf8078e1c4780b9b85f6d037fbd5524d12e2037
gmpassos <gracilianomp@gmail.com>
2022-08-02 04:42:11 -0300

    v1.2.4 - `TypeParser` and `TypeInfo`:   - Add support for `Uint8List`.

---
+++ example/reflection_factory_example.reflection.g.dart
@@ -0,0 +1,267 @@
+//
+// GENERATED CODE - DO NOT MODIFY BY HAND!
+// BUILDER: reflection_factory/1.2.4
+// BUILD COMMAND: dart run build_runner build
+//
+
+// ignore_for_file: unnecessary_const
+// ignore_for_file: unnecessary_cast
+// ignore_for_file: unnecessary_type_check
+
+part of 'reflection_factory_example.dart';
+
+// ignore: non_constant_identifier_names
+User User$fromJson(Map<String, Object?> map) =>
+    User$reflection.staticInstance.fromJson(map);
+// ignore: non_constant_identifier_names
+User User$fromJsonEncoded(String jsonEncoded) =>
+    User$reflection.staticInstance.fromJsonEncoded(jsonEncoded);
+
+class User$reflection extends ClassReflection<User> {
+  User$reflection([User? object]) : super(User, object);
+
+  static bool _registered = false;
+  @override
+  void register() {
+    if (!_registered) {
+      _registered = true;
+      super.register();
+      _registerSiblingsReflection();
+    }
+  }
+
+  @override
+  Version get languageVersion => Version.parse('2.15.0');
+
+  @override
+  Version get reflectionFactoryVersion => Version.parse('1.2.4');
+
+  @override
+  User$reflection withObject([User? obj]) => User$reflection(obj);
+
+  static User$reflection? _withoutObjectInstance;
+  @override
+  User$reflection withoutObjectInstance() => _withoutObjectInstance ??=
+      super.withoutObjectInstance() as User$reflection;
+
+  static User$reflection get staticInstance =>
+      _withoutObjectInstance ??= User$reflection();
+
+  @override
+  User$reflection getStaticInstance() => staticInstance;
+
+  static bool _boot = false;
+  static void boot() {
+    if (_boot) return;
+    _boot = true;
+    User$reflection.staticInstance;
+  }
+
+  @override
+  bool get hasDefaultConstructor => false;
+  @override
+  User? createInstanceWithDefaultConstructor() => null;
+
+  @override
+  bool get hasEmptyConstructor => true;
+  @override
+  User? createInstanceWithEmptyConstructor() => User.empty();
+  @override
+  bool get hasNoRequiredArgsConstructor => true;
+  @override
+  User? createInstanceWithNoRequiredArgsConstructor() => User.empty();
+
+  @override
+  List<String> get constructorsNames => const <String>['', 'empty'];
+
+  @override
+  ConstructorReflection<User>? constructor<R>(String constructorName) {
+    var lc = constructorName.trim().toLowerCase();
+
+    switch (lc) {
+      case '':
+        return ConstructorReflection<User>(
+            this,
+            User,
+            '',
+            () => (String? email, String pass) => User(email, pass),
+            const <ParameterReflection>[
+              ParameterReflection(
+                  TypeReflection.tString, 'email', true, true, null, null),
+              ParameterReflection(
+                  TypeReflection.tString, 'pass', false, true, null, null)
+            ],
+            null,
+            null,
+            null);
+      case 'empty':
+        return ConstructorReflection<User>(this, User, 'empty',
+            () => () => User.empty(), null, null, null, null);
+      default:
+        return null;
+    }
+  }
+
+  @override
+  List<Object> get classAnnotations => List<Object>.unmodifiable(<Object>[]);
+
+  @override
+  List<ClassReflection> siblingsClassReflection() =>
+      _siblingsReflection().whereType<ClassReflection>().toList();
+
+  @override
+  List<Reflection> siblingsReflection() => _siblingsReflection();
+
+  @override
+  List<Type> get supperTypes => const <Type>[];
+
+  @override
+  bool get hasMethodToJson => false;
+
+  @override
+  Object? callMethodToJson([User? obj]) => null;
+
+  @override
+  List<String> get fieldsNames => const <String>['email', 'hasEmail', 'pass'];
+
+  @override
+  FieldReflection<User, T>? field<T>(String fieldName, [User? obj]) {
+    obj ??= object;
+
+    var lc = fieldName.trim().toLowerCase();
+
+    switch (lc) {
+      case 'email':
+        return FieldReflection<User, T>(
+          this,
+          User,
+          TypeReflection.tString,
+          'email',
+          true,
+          (o) => () => o!.email as T,
+          (o) => (T? v) => o!.email = v as String?,
+          obj,
+          false,
+          false,
+          null,
+        );
+      case 'pass':
+        return FieldReflection<User, T>(
+          this,
+          User,
+          TypeReflection.tString,
+          'pass',
+          false,
+          (o) => () => o!.pass as T,
+          (o) => (T? v) => o!.pass = v as String,
+          obj,
+          false,
+          false,
+          null,
+        );
+      case 'hasemail':
+        return FieldReflection<User, T>(
+          this,
+          User,
+          TypeReflection.tBool,
+          'hasEmail',
+          false,
+          (o) => () => o!.hasEmail as T,
+          null,
+          obj,
+          false,
+          false,
+          null,
+        );
+      default:
+        return null;
+    }
+  }
+
+  @override
+  List<String> get staticFieldsNames => const <String>[];
+
+  @override
+  FieldReflection<User, T>? staticField<T>(String fieldName) {
+    return null;
+  }
+
+  @override
+  List<String> get methodsNames => const <String>['checkPassword'];
+
+  @override
+  MethodReflection<User, R>? method<R>(String methodName, [User? obj]) {
+    obj ??= object;
+
+    var lc = methodName.trim().toLowerCase();
+
+    switch (lc) {
+      case 'checkpassword':
+        return MethodReflection<User, R>(
+            this,
+            User,
+            'checkPassword',
+            TypeReflection.tBool,
+            false,
+            (o) => o!.checkPassword,
+            obj,
+            false,
+            const <ParameterReflection>[
+              ParameterReflection(
+                  TypeReflection.tString, 'pass', false, true, null, null)
+            ],
+            null,
+            null,
+            null);
+      default:
+        return null;
+    }
+  }
+
+  @override
+  List<String> get staticMethodsNames => const <String>[];
+
+  @override
+  MethodReflection<User, R>? staticMethod<R>(String methodName) {
+    return null;
+  }
+}
+
+extension User$reflectionExtension on User {
+  /// Returns a [ClassReflection] for type [User]. (Generated by [ReflectionFactory])
+  ClassReflection<User> get reflection => User$reflection(this);
+
+  /// Returns a JSON for type [User]. (Generated by [ReflectionFactory])
+  Object? toJson({bool duplicatedEntitiesAsID = false}) =>
+      reflection.toJson(null, null, duplicatedEntitiesAsID);
+
+  /// Returns a JSON [Map] for type [User]. (Generated by [ReflectionFactory])
+  Map<String, dynamic>? toJsonMap({bool duplicatedEntitiesAsID = false}) =>
+      reflection.toJsonMap(duplicatedEntitiesAsID: duplicatedEntitiesAsID);
+
+  /// Returns an encoded JSON [String] for type [User]. (Generated by [ReflectionFactory])
+  String toJsonEncoded(
+          {bool pretty = false, bool duplicatedEntitiesAsID = false}) =>
+      reflection.toJsonEncoded(
+          pretty: pretty, duplicatedEntitiesAsID: duplicatedEntitiesAsID);
+
+  /// Returns a JSON for type [User] using the class fields. (Generated by [ReflectionFactory])
+  Object? toJsonFromFields({bool duplicatedEntitiesAsID = false}) => reflection
+      .toJsonFromFields(duplicatedEntitiesAsID: duplicatedEntitiesAsID);
+}
+
+List<Reflection> _listSiblingsReflection() => <Reflection>[
+      User$reflection(),
+    ];
+
+List<Reflection>? _siblingsReflectionList;
+List<Reflection> _siblingsReflection() => _siblingsReflectionList ??=
+    List<Reflection>.unmodifiable(_listSiblingsReflection());
+
+bool _registerSiblingsReflectionCalled = false;
+void _registerSiblingsReflection() {
+  if (_registerSiblingsReflectionCalled) return;
+  _registerSiblingsReflectionCalled = true;
+  var length = _listSiblingsReflection().length;
+  assert(length > 0);
+}
