5757f20298037ef358380f4ba46c1a5dcb478a63
gmpassos <gracilianomp@gmail.com>
2023-03-27 20:43:54 -0300

    v2.0.6 - `TypeParser.parseBool`: improve detected values. - analyzer: ^5.10.0

--- example/reflection_factory_bridge_example.reflection.g.dart
+++ example/reflection_factory_bridge_example.reflection.g.dart
@@ -1,6 +1,6 @@
 //
 // GENERATED CODE - DO NOT MODIFY BY HAND!
-// BUILDER: reflection_factory/2.0.5
+// BUILDER: reflection_factory/2.0.6
 // BUILD COMMAND: dart run build_runner build
 //
 
@@ -17,7 +17,7 @@ typedef __TI<T> = TypeInfo<T>;
 typedef __PR = ParameterReflection;
 
 mixin __ReflectionMixin {
-  static final Version _version = Version.parse('2.0.5');
+  static final Version _version = Version.parse('2.0.6');
 
   Version get reflectionFactoryVersion => _version;
 
