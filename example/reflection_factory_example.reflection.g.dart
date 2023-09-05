36e84a25e94554e0f6ad2932ef96c04e2029aeea
gmpassos <gracilianomp@gmail.com>
2023-09-05 20:05:29 -0300

    update generated reflection

--- example/reflection_factory_example.reflection.g.dart
+++ example/reflection_factory_example.reflection.g.dart
@@ -1,12 +1,14 @@
 //
 // GENERATED CODE - DO NOT MODIFY BY HAND!
-// BUILDER: reflection_factory/2.2.0
+// BUILDER: reflection_factory/2.2.1
 // BUILD COMMAND: dart run build_runner build
 //
 
 // coverage:ignore-file
 // ignore_for_file: unused_element
 // ignore_for_file: no_leading_underscores_for_local_identifiers
+// ignore_for_file: camel_case_types
+// ignore_for_file: camel_case_extensions
 // ignore_for_file: unnecessary_const
 // ignore_for_file: unnecessary_cast
 // ignore_for_file: unnecessary_type_check
@@ -18,7 +20,7 @@ typedef __TI<T> = TypeInfo<T>;
 typedef __PR = ParameterReflection;
 
 mixin __ReflectionMixin {
-  static final Version _version = Version.parse('2.2.0');
+  static final Version _version = Version.parse('2.2.1');
 
   Version get reflectionFactoryVersion => _version;
 
